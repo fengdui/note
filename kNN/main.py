e2c2d712105de2f6fd61e0c296c0aa109375bee5
henyumen <fdfinal3>
2016-02-03 19:27:20 +0800

    Initial commit

---
+++ kNN/main.py
@@ -0,0 +1,13 @@
+import kNN
+import scipy
+import matplotlib
+import matplotlib.pyplot as plt
+
+datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
+print(datingDataMat)
+print(datingLabels)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2])
+plt.show()
