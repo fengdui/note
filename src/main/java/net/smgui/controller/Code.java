5f12e278273646d4b878837a2ce6b9c7485f9508
henyumen <fdfinal3>
2016-02-14 17:53:11 +0800

    2016/02/14

--- src/main/java/net/smgui/controller/Code.java
+++ src/main/java/net/smgui/controller/Code.java
@@ -1,12 +1,25 @@
 package net.smgui.controller;
 
+import net.smgui.entity.Result;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class Code {
 	@RequestMapping("/code")
 	public String gotoCode() {
+		System.out.println("aaaaa");
 		return "code";
+	}
+
+	@RequestMapping(value = "/compile_java", method = RequestMethod.POST)
+	public Result CompileJava(HttpServletRequest request) {
+		String path = request.getSession().getServletContext().getRealPath("/file/java");
+//		String msg = Runtime.getRuntime().exec();
+//		return new Result(msg);
+		return new Result(path);
 	}
 }
---
+++ src/main/java/net/smgui/entity/Result.java
@@ -0,0 +1,14 @@
+package net.smgui.entity;
+
+/**
+ * Result
+ *
+ * @author FD
+ * @date 2016/2/14 0014
+ */
+public class Result {
+    private String msg;
+    public Result(String msg) {
+        this.msg = msg;
+    }
+}
