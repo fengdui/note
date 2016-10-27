92e6e4348842313a85651bdb56829420b91b56d8
fengdui <614173971@qq.com>
2016-10-27 13:11:28 +0800

    spring boot

--- rpc/src/main/java/com/zheyue/rpc/annotation/RpcService.java
+++
@@ -1,7 +0,0 @@
-package com.zheyue.rpc.annotation;
-
-/**
- * Created by fd on 2016/10/25.
- */
-public @interface RpcService {
-}
--- rpc/src/main/java/com/zheyue/rpc/common/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.zheyue.rpc.common;
-
-public interface Constant {
-
-	int ZK_SESSION_TIMEOUT = 5000;
-
-    String ZK_REGISTRY_PATH = "/zookeeper/quota";
-    String ZK_DATA_PATH = ZK_REGISTRY_PATH + "/data";
-}
