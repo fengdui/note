64ab8080038abb0ad56b969b77f9bc04e75fb8d2
fengdui <614173971@qq.com>
2016-10-31 11:52:46 +0800

    mybatis

---
+++ zheyue-security/src/main/java/com/zheyue/security/ZheyueSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.zheyue.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZheyueSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZheyueSecurityApplication.class, args);
+	}
+}
