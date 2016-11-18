af04e3cc0d262f6f1556a51c40f8eaa6908d7bc1
FD <614173971@qq.com>
2016-11-18 17:22:21 +0800

    spring security

--- zheyue-security/src/main/java/com/zheyue/security/config/WebSecurityConfig.java
+++ zheyue-security/src/main/java/com/zheyue/security/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.zheyue.security.config;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -7,4 +8,33 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * @date 2016/11/18
  */
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 设置拦截规则
+        // 自定义accessDecisionManager访问控制器,并开启表达式语言
+        http.authorizeRequests()
+                .antMatchers("/", "login").permitAll();
+
+        // 开启默认登录页面
+        // http.formLogin();
+
+        // 自定义登录页面
+        http.csrf().disable().formLogin().loginPage("/login")
+                .failureUrl("/login?error=1")
+                .loginProcessingUrl("/j_spring_security_check")
+                .usernameParameter("j_username")
+                .passwordParameter("j_password").permitAll();
+
+        // 自定义注销
+        http.logout().logoutUrl("/logout").logoutSuccessUrl("/login")
+                .invalidateHttpSession(true);
+
+        // session管理
+        http.sessionManagement().sessionFixation().changeSessionId()
+                .maximumSessions(1).expiredUrl("/");
+
+        // RemeberMe
+        http.rememberMe().key("webmvc#FD637E6D9C0F1A5A67082AF56CE32485");
+
+    }
 }
