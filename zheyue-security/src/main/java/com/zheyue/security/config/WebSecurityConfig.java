ef464bb47205b535255c3a5dfdedd4b752835cb4
FD <614173971@qq.com>
2016-11-18 13:10:32 +0800

    spring security

---
+++ zheyue-security/src/main/java/com/zheyue/security/config/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.zheyue.security.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author FD
+ * @date 2016/11/18
+ */
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+}
