0456cbc12726dfe9899cbad3cc9285040f1bcaed
FD <614173971@qq.com>
2016-11-19 17:04:36 +0800

    spring security

--- zheyue-security/src/main/java/com/zheyue/security/config/WebSecurityConfig.java
+++ zheyue-security/src/main/java/com/zheyue/security/config/WebSecurityConfig.java
@@ -1,29 +1,36 @@
 package com.zheyue.security.config;
 
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @author FD
  * @date 2016/11/18
  */
+
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // 设置拦截规则
         // 自定义accessDecisionManager访问控制器,并开启表达式语言
         http.authorizeRequests()
-                .antMatchers("/", "login").permitAll();
+                .antMatchers("/", "login", "hello").permitAll();
 
         // 开启默认登录页面
         // http.formLogin();
 
         // 自定义登录页面
-        http.csrf().disable().formLogin().loginPage("/login")
-                .failureUrl("/login?error=1")
+        http.formLogin().loginPage("/login")
+                .failureUrl("/error")
                 .loginProcessingUrl("/j_spring_security_check")
-                .usernameParameter("j_username")
-                .passwordParameter("j_password").permitAll();
+                .usernameParameter("username")
+                .passwordParameter("password").permitAll();
 
         // 自定义注销
         http.logout().logoutUrl("/logout").logoutSuccessUrl("/login")
@@ -35,6 +42,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         // RemeberMe
         http.rememberMe().key("webmvc#FD637E6D9C0F1A5A67082AF56CE32485");
-
     }
 }
--- zheyue-security/src/main/java/com/zheyue/security/controller/HelloController.java
+++ zheyue-security/src/main/java/com/zheyue/security/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.zheyue.security.controller;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,12 +12,17 @@ import org.springframework.web.servlet.ModelAndView;
  * @data 2016/11/1
  */
 @RestController
-public class HelloController {
+public class HelloController implements ApplicationContextAware {
 
     @RequestMapping("hello")
     public ModelAndView hello() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("hello");
         return mv;
+    }
+    ApplicationContext applicationContext;
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
