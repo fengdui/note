a41330e16eaf5fb64df474d4b5e3cb284c973559
fengdui <614173971@qq.com>
2016-11-29 11:46:50 +0800

    encrypt

--- src/main/java/com/zheyue/encrypt/config/MybatisConfig.java
+++ src/main/java/com/zheyue/encrypt/config/MybatisConfig.java
@@ -1,6 +1,13 @@
 package com.zheyue.encrypt.config;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * @author FD
@@ -8,6 +15,29 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class MybatisConfig {
+
+    @Autowired
+    DataSource dataSource;
+
+
+    @Bean("sqlSessionFactory")
+    public SqlSessionFactoryBean sqlSessionFactory() {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        return sqlSessionFactoryBean;
+    }
+
+    /**
+     * mybatis自动扫描配置
+     * @return
+     */
+    @Bean("scannerConfigurer")
+    public MapperScannerConfigurer scannerConfigurer() {
+        MapperScannerConfigurer scannerConfigurer = new MapperScannerConfigurer();
+        scannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactory");
+        scannerConfigurer.setBasePackage("com.zheyue.encrypt.dao");
+        return scannerConfigurer;
+    }
 
 
 }
---
+++ src/main/java/com/zheyue/encrypt/entity/JobStatus.java
@@ -0,0 +1,37 @@
+package com.zheyue.encrypt.entity;
+
+import com.zheyue.encrypt.utils.Status;
+
+/**
+ * @author FD
+ * @date 2016/11/29
+ */
+public class JobStatus {
+    private String filePath;
+    private int jobCode = Status.UNSUBMIT.getCode();
+    private String jobMessage = Status.UNSUBMIT.getMsg();
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public int getJobCode() {
+        return jobCode;
+    }
+
+    public void setJobCode(int jobCode) {
+        this.jobCode = jobCode;
+    }
+
+    public String getJobMessage() {
+        return jobMessage;
+    }
+
+    public void setJobMessage(String jobMessage) {
+        this.jobMessage = jobMessage;
+    }
+}
