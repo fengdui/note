f052befb5dc52e48266d2f3be33d3b95b154f99d
fengdui <614173971@qq.com>
2016-12-08 17:11:02 +0800

    encrypt

--- src/main/java/com/zheyue/encrypt/FileEncryptApplication.java
+++ src/main/java/com/zheyue/encrypt/FileEncryptApplication.java
@@ -7,7 +7,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FileEncryptApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FileEncryptApplication.class, args);
+
+	private Thread thread = new Thread(new MyRunnable(), "fd");
+
+	class MyRunnable implements Runnable {
+
+		private volatile boolean stoped = true;
+
+		@Override
+		public void run() {
+			while (!stoped) {
+				try {
+					System.out.println("运行中。。。");
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
+
+	public static void main(String[] args) {
+//		SpringApplication.run(FileEncryptApplication.class, args);
+		new FileEncryptApplication().thread.start();
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("系统退出");
+			}
+		}));
+	}
+
 }
