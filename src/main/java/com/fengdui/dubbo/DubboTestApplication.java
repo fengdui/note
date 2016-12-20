e129104c23c54a15f29bf5ff4e78e3fa108a1cb7
fengdui <614173971@qq.com>
2016-12-20 16:48:53 +0800

    Initial commit

---
+++ src/main/java/com/fengdui/dubbo/DubboTestApplication.java
@@ -0,0 +1,12 @@
+package com.fengdui.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboTestApplication.class, args);
+	}
+}
