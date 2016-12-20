b29c983fbb328587c7329e4c268683503ecc546d
fengdui <614173971@qq.com>
2016-12-20 16:46:20 +0800

    Initial commit

---
+++ src/main/java/com/fengdui/rmq/RmqTestApplication.java
@@ -0,0 +1,12 @@
+package com.fengdui.rmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmqTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmqTestApplication.class, args);
+	}
+}
---
+++ src/main/java/com/fengdui/rmq/producer/Producer.java
@@ -0,0 +1,34 @@
+package com.fengdui.rmq.producer;
+
+/**
+ * @author FD
+ * @date 2016/12/2
+ */
+public class Producer {
+//    public static void main(String[] args) {
+////        System.out.println(Integer.parseInt("C0A8009012C800B4AAC22B758A700000", 16));
+//        DefaultMQProducer producer = new DefaultMQProducer("Producer");
+//        //nameserver服务,多个以;分开
+//        producer.setNamesrvAddr("localhost:9876");
+//        producer.setVipChannelEnabled(false);
+//        try{
+//            producer.start();
+//
+//            Message msg = new Message("PushTopic","push","1","Just for test.".getBytes());
+//            SendResult result = producer.send(msg);
+//            System.out.println("id:"+result.getMsgId()+" result:" +result.getSendStatus());
+//
+////            msg = new Message("PushTopic","push","2","Just for test.".getBytes());
+////            result = producer.send(msg);
+////            System.out.println("id:"+result.getMsgId()+" result:" +result.getSendStatus());
+////
+////            msg = new Message("PullTopic","pull","1","Just for test.".getBytes());
+////            result = producer.send(msg);
+////            System.out.println("id:"+result.getMsgId()+" result:" +result.getSendStatus());
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }finally {
+//            producer.shutdown();
+//        }
+//    }
+}
