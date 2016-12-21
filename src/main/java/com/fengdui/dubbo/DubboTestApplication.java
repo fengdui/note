5ea55edb93a078763d56f70d18bc816d05dc0585
fengdui <614173971@qq.com>
2016-12-21 10:52:46 +0800

    dubbo

--- src/main/java/com/fengdui/dubbo/DubboTestApplication.java
+++ src/main/java/com/fengdui/dubbo/DubboTestApplication.java
@@ -2,11 +2,16 @@ package com.fengdui.dubbo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DubboTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DubboTestApplication.class, args);
+//		SpringApplication.run(DubboTestApplication.class, args);
+		SpringApplication application = new SpringApplication();
+		application.setWebEnvironment(false);
+		ConfigurableApplicationContext applicationContext = application.run(DubboTestApplication.class, args);
+		applicationContext.getBean("iDubboDemoService");
 	}
 }
