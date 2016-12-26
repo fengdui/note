8334c4da0fb99ba0a1c7dc6c3e63cea0f586060e
fengdui <614173971@qq.com>
2016-12-26 12:09:27 +0800

    Initial commit

---
+++ src/main/java/com/fengdui/dubbo/service/DubboDemoServiceImpl.java
@@ -0,0 +1,22 @@
+package com.fengdui.dubbo.service;
+
+import com.alibaba.dubbo.config.annotation.Service;
+
+/**
+ * @author FD
+ * @date 2016/12/20
+ */
+@Service(version="1.0.0")
+public class DubboDemoServiceImpl implements IDubboDemoService{
+
+    @Override
+    public String sayHello(String name) {
+        return "hello " + name;
+    }
+
+    @Override
+    public String sayYourAge(int age) {
+        return null;
+    }
+
+}
