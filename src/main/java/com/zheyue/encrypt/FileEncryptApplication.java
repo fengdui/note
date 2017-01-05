1bbe098bd09fda3d0eed6160b2bb92770f51faac
fengdui <614173971@qq.com>
2017-01-05 17:10:20 +0800

    test

--- src/main/java/com/zheyue/encrypt/FileEncryptApplication.java
+++ src/main/java/com/zheyue/encrypt/FileEncryptApplication.java
@@ -1,7 +1,6 @@
 package com.zheyue.encrypt;
 
 import com.zheyue.encrypt.server.FileServer;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
