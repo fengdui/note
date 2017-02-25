acc7e7eb26f631db85a883296a196d672d98ef57
fengdui <614173971@qq.com>
2017-02-25 14:05:23 +0800

    java8

--- src/main/java/com/fengdui/java8/Java8Application.java
+++ src/main/java/com/fengdui/java8/Java8Application.java
@@ -3,10 +3,15 @@ package com.fengdui.java8;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class Java8Application {
 
 	public static void main(String[] args) {
+
+		List<String> a;
+		a.stream()
 		SpringApplication.run(Java8Application.class, args);
 	}
 }
---
+++ src/main/java/com/fengdui/java8/Stream_test.java
@@ -0,0 +1,8 @@
+package com.fengdui.java8;
+
+/**
+ * @author FD
+ * @date 2017/2/25
+ */
+public class Stream_test {
+}
