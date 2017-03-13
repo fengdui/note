0d562df3a5bb2fb43b9ade08c1bf8049dbf2623c
fengdui <614173971@qq.com>
2017-03-13 18:51:28 +0800

    Initial commit

---
+++ src/main/java/com/fengdui/extjs/FengduiExtjsApplication.java
@@ -0,0 +1,12 @@
+package com.fengdui.extjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FengduiExtjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FengduiExtjsApplication.class, args);
+	}
+}
