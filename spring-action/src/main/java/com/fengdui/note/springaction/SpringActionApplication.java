9e6728459608ad8208513f2706670282720fb2e0
fengdui <614173971@qq.com>
2017-10-13 11:34:08 +0800

    合并spring实战

---
+++ spring-action/src/main/java/com/fengdui/note/springaction/SpringActionApplication.java
@@ -0,0 +1,12 @@
+package com.fengdui.note.springaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActionApplication.class, args);
+	}
+}
